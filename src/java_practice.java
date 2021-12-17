99da064e921f872c5ff9179d4dc42a821f9e2e83
Nicholas <nicholasdiazjr509@gmail.com>
2021-12-17 16:50:47 -0600

    some practice

---
+++ src/java_practice.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class java_practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner stdin = new Scanner(System.in);
+        for (int j = 0; j < 3; ++j) {
+            int b = sc.nextInt();
+            System.out.println(b);
+        }
+            for (int i = 0; i < 3; ++i) {
+                int a = stdin.nextInt();
+                    System.out.println(a);
+
+//                System.out.println(sc.nextInt());
+//                System.out.println(sc.nextInt());
+//                System.out.println(sc.nextInt());
+
+//   Task
+//In this challenge, you must read
+//integers from stdin and then print them to stdout.
+// Each integer must be printed on a new line. To make the problem a little easier,
+// a portion of the code is provided for you in the editor below.
+//Input Format
+//There are lines of input, and each line contains a single integer.
+//Sample Input
+//42
+//100
+//125
+            }
+        }
+    }
